83c28b1d88ea5af94f8870f08147985e59d56a45
Mantulis00 <mantas.radzevicius1@gmail.com>
2020-07-21 20:21:13 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+//#include <fstream>
+
+using namespace std;
+
+// leave one or remove all
+
+
+int main()
+{
+
+    bool looser =true, swaped = false;
+    long n, k, pile ;
+    long piles[100010];
+
+    //fstream fs ("duom.txt");
+
+    cin >> n;
+
+for (int o = 0; o<n; o++)
+{
+    looser =true;
+    swaped = false;
+    cin >> k;
+
+    for (long x=0; x<k; x++)
+    {
+        cin >> pile;
+        piles[x] = pile;
+
+    }
+
+
+    for (long x=k-2; x>=0; x--)
+    {
+        if (looser)
+        {
+            looser = !looser;
+        }
+
+        else
+           {
+            looser = !looser;
+        }
+
+        if (piles[x] > 1 && !looser)
+        {
+    swaped = true;
+                if (looser)
+            {
+                looser = !looser;
+            }
+
+            else
+               {
+                looser = !looser;
+            }
+        }
+
+    }
+
+    if (looser) cout << "First" << endl;
+    else cout << "Second" << endl;
+}
+
+    return 0;
+}
